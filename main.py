ef557b4af5d3e3316dc8b469b821521a22ab2f8c
Eugene Luckyanets <leugenea@gmail.com>
2012-04-08 20:46:41 +0400

    Main reads file

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import wave, sys, struct
+from numpy import zeros, array, hamming
+
+wavename = "file.mp3.wav"
+fft_length = 1024
+sample_width = 4
+format = "I"
 
 def main():
-  pass
+  input = wave.open(wavefile, 'rb')
+  sample_rate = input.getframerate()
+  samples = input.getnframes()
+  num_of_ffts = (samples / int(fft_length)) - 2
+  temp = zeros((num_of_ffts, fft_length)) 
+  
+  for i in range(num_of_ffts):
+    temp(i, :) = array(struct.unpack(("%d" %(fft_length))+format), input.readframes(fft_length), float) - 2**(8*sample_width)
 
 if __name__ == "__main__":
   main()
--- wavetest.py
+++ wavetest.py
@@ -4,10 +4,11 @@ import array, wave
 from numpy.fft import *
 
 def main():
-  file = wave.open("file.wav", 'r')
+  file = wave.open("file.mp3.wav", 'r')
   params = file.getparams()
+  print params
   frames = array.array('B', file.readframes(100000))
-  print frames
+  #print frames
   out = wave.open("out.wav", 'w')
   out.setparams(params)
   out.writeframes(frames.tostring())
