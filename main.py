af2e0deaa5e649ba9ec2c0506cee1561978c1743
Eugene Luckyanets <leugenea@gmail.com>
2012-04-08 21:42:29 +0400

    HUGE bugfix (in struct.unpack)

--- main.py
+++ main.py
@@ -3,25 +3,28 @@ from numpy import zeros, array, hamming, abs, log10
 from numpy.fft import rfft
 
 wavefile = "file.mp3.wav"
-fft_length = 1024
+fft_length_in_frames = 1024
 sample_width = 4
+ch_num = 2
 format = "I"
 
 def main():
   input = wave.open(wavefile, 'rb')
   sample_rate = input.getframerate()
   samples = input.getnframes()
-  num_of_ffts = (samples / int(fft_length)) - 2
-  temp = zeros((num_of_ffts, fft_length), float) 
+  ch_num = input.getnchannels()
+  fft_length_in_format = fft_length_in_frames * ch_num
+  num_of_ffts = (samples / int(fft_length_in_frames)) - 2
+  temp = zeros((num_of_ffts, fft_length_in_format), float) 
   
   for i in range(num_of_ffts):
-    temp[i, :] = array(struct.unpack(("%d" %(fft_length))+format, input.readframes(fft_length)), float)- 2**(8*sample_width)
+    temp[i, :] = array(struct.unpack(("%d" %(fft_length_in_format))+format, input.readframes(fft_length_in_frames)), float)- 2**(8*sample_width)
   
-  ham = hamming(fft_length)
+  ham = hamming(fft_length_in_format)
   for i in range(num_of_ffts):
     temp[i, :] = temp[i, :] * ham
   
-  powers = zeros((num_of_ffts, fft_length), float)
+  powers = zeros((num_of_ffts, fft_length_in_format), float)
   
   for i in range(num_of_ffts):
     powers[i, :] = rfft(temp[i, :])
